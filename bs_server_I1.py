1ad9331577bfd22fbac90f550dbf984c848403e5
Diane SAUTEREAU DU PART <diane.sautereau@ynov.com>
2024-10-11 09:34:47 +0200

    I1

---
+++ bs_server_I1.py
@@ -0,0 +1,28 @@
+import socket
+
+host = '' 
+port = 13337 
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))  
+
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+conn.sendall(b'Hi mate !')
+
+while True : 
+    try:
+        data = conn.recv(1024)
+
+        if not data: break
+
+        print(f"Réponse du client : {data}")
+
+    except socket.error:
+        print("Error occured")
+        break
+
+
+conn.close()
+
